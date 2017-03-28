970f627e36609938d457525dcf67ae90590c5f5a
David Emerick <d.emerick@outlook.com>
2017-03-28 08:40:42 -0700

    js scroll optional assignment

--- WebFundamentals/Javascript/studentsAndInstructors.js
+++ WebFundamentals/Javascript/studentsAndInstructors.js
@@ -1,3 +1,47 @@
+function directory(users){
+ 
+  // Create array of keys in users: Students & Instructors
+  var userTypes = Object.keys(users);
+  var userType1 = userTypes[0];
+  var userType2 = userTypes[1];
+  
+     // enumerates keys in users: Students & Instructors
+    for(var key in users){
+      console.log(key);
+        var arr = users[key];
+        // enumerates properties in array
+        var counter = 1;
+        for(var prop in arr){
+            console.log(counter + " " + arr[prop].first_name + " " + arr[prop].last_name + " " + (arr[prop].first_name.length + arr[prop].last_name.length));
+        counter++;
+        }
+    }
+
+}
+
+var users = {
+ 'Students': [ 
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+  ],
+ 'Instructors': [
+     {first_name : 'Michael', last_name : 'Choi'},
+     {first_name : 'Martin', last_name : 'Puryear'}
+  ]
+ }
+
+directory(users);
+
+
+
+
+
+
+
+
+/*
 function enumArr(students){
     for(var i in students){
       console.log(students[i].first_name + " " + students[i].last_name);
@@ -74,4 +118,5 @@ function enumArrOverThink(arrOfObjs){
         console.log(firstObj + " " + secondObj);
     }
 }
+*/
 
